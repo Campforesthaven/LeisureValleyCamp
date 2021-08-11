54f2e535beb9c83d72927fe60835088ef78091e6
LeisureValleyCamp <88438832+LeisureValleyCamp@users.noreply.github.com>
2021-08-10 23:47:56 -0400

    test update for php form handler

--- php/mail.php
+++ php/mail.php
@@ -13,7 +13,7 @@ $to = "kumarorlewicz@gmail.com";
 $subject = "New Submission from Leisure Valley Contact Us Form";
 $txt ="Name = ". $name . "\r\n  Email = " . $email . "\r\n Phone = " . $number . "\r\n Message =" . $message;
 
-$headers = "From: noreply@yoursite.com" . "\r\n" .
+$headers = "From: noreply@leisurevalley.com" . "\r\n" .
 "CC: somebodyelse@example.com";
 if($email!=NULL){
     mail($to,$subject,$txt,$headers);
